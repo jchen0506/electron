3e9de6806aa701b0b3f0f3a3589dab943f573866
Zach Ayers <z.ayers@icloud.com>
2020-07-01 19:02:42 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 /* Require the electron module */
 const electron = require("electron");
 
-/* Destructure off the 'app' process from electron */
+/* Destructure off the 'app' process & 'BrowserWindow' from electron */
 const { app, BrowserWindow } = electron;
 
+/* Once the app is ready for connection, start up a new window with the contents of the 'index.html' file */
 app.on("ready", () => {
   const mainWindow = new BrowserWindow();
   return mainWindow.loadFile("index.html");
