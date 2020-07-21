6d188b8ae61c03447887afc9c80d2f6434ba2d9a
Zach Ayers <zayers.dev@gmail.com>
2020-07-21 10:01:02 -0500

    init

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-/* Require the electron module */
+// Require the electron module
 const electron = require("electron");
+const path = require('path');
 
-/* Destructure off the 'app' process & 'BrowserWindow' from electron */
-const { app, BrowserWindow } = electron;
+// Destructure off the 'app' process & 'BrowserWindow' from electron
+const { app, BrowserWindow, Tray } = electron;
 
-/* Once the app is ready for connection, start up a new window with the contents of the 'index.html' file */
+// Once the app is ready for connection, start up a new window with the contents of the 'index.html' file
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow();
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+    }
+  });
+
+  //  Add a tray icon!
+  // const iconPath = path.join(__dirname, './icons/iconTemplate@2x.png');
+  // const tray = new Tray(iconPath);
+
   return mainWindow.loadFile("index.html");
 });
+
+
+// Load up a web page inside of the electron app!
+// app.on("ready", () => {
+//   const mainWindow = new BrowserWindow();
+//   return mainWindow.loadURL("https://www.github.com");
+// });
+
